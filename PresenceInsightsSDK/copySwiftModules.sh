fab6de463d1e1839f3de4d1c7db2ae8d3f1992cd
Kyle Craig <craigjk@kyles-mbp.austin.ibm.com>
2015-08-06 16:48:03 -0500

    Modified copy script to also clean up output directory. Output directory now only contains the built universal framework

--- PresenceInsightsSDK/copySwiftModules.sh
+++ PresenceInsightsSDK/copySwiftModules.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cp Output/PresenceInsightsSDK-Debug-iphonesimulator/PresenceInsightsSDK.framework/Modules/PresenceInsightsSDK.swiftmodule/* Output/PresenceInsightsSDK-Debug-iphoneuniversal/PresenceInsightsSDK.framework/Modules/PresenceInsightsSDK.swiftmodule/
+mv Output/PresenceInsightsSDK-Debug-iphoneuniversal/PresenceInsightsSDK.framework Output/
+rm -r Output/PresenceInsightsSDK-Debug-iphoneos
+rm -r Output/PresenceInsightsSDK-Debug-iphonesimulator
+rm -r Output/PresenceInsightsSDK-Debug-iphoneuniversal
