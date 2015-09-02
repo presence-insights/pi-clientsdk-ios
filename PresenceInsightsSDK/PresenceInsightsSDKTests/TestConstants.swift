3d04d4432ccd44578b452b3986129bb09a96e268
CIARAN E. HANNIGAN <cehannig@us.ibm.com>
2015-09-01 23:04:13 -0400

    86995: removed the codes within the testing constants. Put an explanation at the top of the struct to explain how to use it.

--- PresenceInsightsSDK/PresenceInsightsSDKTests/TestConstants.swift
+++ PresenceInsightsSDK/PresenceInsightsSDKTests/TestConstants.swift
@@ -16,13 +16,16 @@
 
 import Foundation
 
+/**
+* This struct stores the Presence Insights codes used in the unit tests.
+* Add the codes of your tenant to this file when testing the framework.
+*/
 struct PI {
-    static let Hostname = "https://presenceinsights.ng.bluemix.net"
-    static let Tenant = "5e5l01lq"
-    static let Org = "el1l208y9"
-    static let Username = "jh1jt0byv"
-    static let Password = "iGG4CWZ-jrhT"
-    
-    static let Site = "zf1l30816"
-    static let Floor = "rd1k40bnm"
+    static let Hostname = ""
+    static let Username = ""
+    static let Password = ""
+    static let Tenant = ""
+    static let Org = ""
+    static let Site = ""
+    static let Floor = ""
 }
