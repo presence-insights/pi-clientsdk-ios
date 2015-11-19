e7f54c0dae319a088ed5bb12bf7f4c88523d9f57
CIARAN E. HANNIGAN <cehannig@us.ibm.com>
2015-11-19 10:03:55 -0500

    added license header to Region Manager

--- PresenceInsightsSDK/PresenceInsightsSDK/RegionManager.swift
+++ PresenceInsightsSDK/PresenceInsightsSDK/RegionManager.swift
@@ -1,10 +1,21 @@
-//
-//  RegionManager.swift
-//  PresenceInsightsSDK
-//
-//  Created by Ciaran Hannigan on 11/2/15.
-//  Copyright © 2015 IBM MIL. All rights reserved.
-//
+/**
+*  PresenceInsightsSDK
+*  RegionManager.swift
+*
+*  Object to contain all zone information.
+*
+*  © Copyright 2015 IBM Corp.
+*
+*  Licensed under the Presence Insights Client iOS Framework License (the "License");
+*  you may not use this file except in compliance with the License. You may find
+*  a copy of the license in the license.txt file in this package.
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+**/
 
 import Foundation
 import CoreLocation
