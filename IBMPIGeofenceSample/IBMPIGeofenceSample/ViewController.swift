6597f5811a5708a7b0172aae1839abadb6e3c8f4
lizeray <lizeray@fr.ibm.com>
2016-03-17 15:08:35 +0100

    basic sample initial commit

---
+++ IBMPIGeofenceSample/IBMPIGeofenceSample/ViewController.swift
@@ -0,0 +1,37 @@
+/**
+*  IBMPIGeofenceSample
+*  ViewController.swift
+*
+*  Performs all communication to the PI Rest API.
+*
+*  © Copyright 2016 IBM Corp.
+*
+*  Licensed under the Presence Insights Client iOS Framework License (the "License");
+*  you may not use this file except in compliance with the License. You may find
+*  a copy of the license in the license.txt file in this package.
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+**/
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
