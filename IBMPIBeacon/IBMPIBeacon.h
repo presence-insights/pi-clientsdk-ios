e88cd7cd873f63390e9614d3b4bf66520627c5d6
CIARAN E. HANNIGAN <cehannig@us.ibm.com>
2016-04-04 19:08:37 -0400

    created PresenceInsightsSDK workspace, and created two new projects IBMPICore and IBMPIBeacon

---
+++ IBMPIBeacon/IBMPIBeacon.h
@@ -0,0 +1,19 @@
+//
+//  IBMPIBeacon.h
+//  IBMPIBeacon
+//
+//  Created by Ciaran Hannigan on 4/4/16.
+//  Copyright © 2016 Ciaran Hannigan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBMPIBeacon.
+FOUNDATION_EXPORT double IBMPIBeaconVersionNumber;
+
+//! Project version string for IBMPIBeacon.
+FOUNDATION_EXPORT const unsigned char IBMPIBeaconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBMPIBeacon/PublicHeader.h>
+
+
---
+++ IBMPICore/IBMPICore.h
@@ -0,0 +1,19 @@
+//
+//  IBMPICore.h
+//  IBMPICore
+//
+//  Created by Ciaran Hannigan on 4/4/16.
+//  Copyright © 2016 Ciaran Hannigan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBMPICore.
+FOUNDATION_EXPORT double IBMPICoreVersionNumber;
+
+//! Project version string for IBMPICore.
+FOUNDATION_EXPORT const unsigned char IBMPICoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBMPICore/PublicHeader.h>
+
+
